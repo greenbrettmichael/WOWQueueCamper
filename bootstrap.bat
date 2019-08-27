d6035ff2c08758a48834a31e7dc6073a3ae48de5
Brett Green <me@brettmichaelgreen.com>
2019-08-27 13:31:14 -0400

    setup bootstrapping for full setup

---
+++ bootstrap.bat
@@ -0,0 +1,6 @@
+git clone https://github.com/microsoft/vcpkg.git
+cd vcpkg
+git pull
+call bootstrap-vcpkg.bat
+.\vcpkg.exe update
+.\vcpkg.exe install tesseract boost-program-options
--- build.bat
+++ build.bat
@@ -1,3 +1,4 @@
 mkdir build
 cd build
-cmake .. -DCMAKE_GENERATOR_PLATFORM=x64 -DCMAKE_TOOLCHAIN_FILE=C:/vcpkg/scripts/buildsystems/vcpkg.cmake
+cmake .. -DCMAKE_GENERATOR_PLATFORM=x64 -DCMAKE_TOOLCHAIN_FILE=vcpkg/scripts/buildsystems/vcpkg.cmake
+cmake --build . --target WOWQueueCamper --config Release
