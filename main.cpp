41a2f81697b4f3511ae509642125afba097cbdb7
Brett Green <me@brettmichaelgreen.com>
2019-08-27 00:07:15 -0400

    setup outline for alarm

--- main.cpp
+++ main.cpp
@@ -1,4 +1,43 @@
+#include <Windows.h>
+#pragma comment(lib, "winmm.lib")
+
+bool checkScreen()
+{
+	// TODO get queue number
+	//HDC hdc = GetDC(NULL); // get the desktop device context
+	//HDC hDest = CreateCompatibleDC(hdc); // create a device context to use yourself
+
+	//// get the height and width of the screen
+	//int height = GetSystemMetrics(SM_CYVIRTUALSCREEN);
+	//int width = GetSystemMetrics(SM_CXVIRTUALSCREEN);
+
+	//// create a bitmap
+	//HBITMAP hbDesktop = CreateCompatibleBitmap(hdc, width, height);
+
+	//// use the previously created device context with the bitmap
+	//SelectObject(hDest, hbDesktop);
+
+	//// copy from the desktop device context to the bitmap device context
+	//// call this once per 'frame'
+	//BitBlt(hDest, 0, 0, width, height, hdc, 0, 0, SRCCOPY);
+
+	//// after the recording is done, release the desktop context you got..
+	//ReleaseDC(NULL, hdc);
+
+	//// ..delete the bitmap you were using to capture frames..
+	//DeleteObject(hbDesktop);
+
+	//// ..and delete the context you created
+	//DeleteDC(hDest);
+	return true;
+}
+
 int main()
 {
+	while (!checkScreen())
+	{
+		Sleep(1000);
+	}
+	PlaySound("default.wav", NULL, SND_SYNC);
     return 0;
 }
